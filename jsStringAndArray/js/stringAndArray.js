d19cee789e8ccba9359adf3e3d8caf5034b33b7b
askalerion <bednartd@rose-hulman.edu>
2017-09-22 13:54:38 -0400

    initial commit

--- jsStringAndArray/js/stringAndArray.js
+++ jsStringAndArray/js/stringAndArray.js
@@ -5,14 +5,75 @@
     You may work with your in-class partner.
 */
 
-function isBlank(input) { 
-    return false;
+function isBlank(input) {
+    return input.length <= 0;
 }
 
 function stringToArray(input) {
-    return null;
+    return input.split(' ');
 }
 
 function abbreviateName(name) {
-    return null;    
+    let nameList = name.split(' ');
+    let output = '';
+
+    let firstName = nameList[0];
+    let lastName = nameList[1].substring(0, 1) + '.';
+
+    return firstName + ' ' + lastName;
+}
+
+function titleCase(input) {
+    let list = input.split(' ');
+    let output = '';
+
+    for (let i = 0; i < list.length; i++) {
+        output += list[i][0].toUpperCase() + list[i].substring(1);
+
+        if (i + 1 < list.length) {
+            output += ' ';
+        }
+    }
+
+    return output;
+}
+
+function firstN(array, n) {
+    if (n) {
+        if (n < array.length) {
+            return array.slice(0, n);
+        }
+        return array;
+    } else {
+        return array[0];
+    }
+}
+
+function lastN(array, n) {
+    if (n) {
+        if (n < array.length) {
+            array.splice(0, array.length - n);
+        }
+        return array;
+    } else {
+        return array[array.length - 1];
+    }
+}
+
+function sumPair(numbers, target) {
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i - 1] + numbers[i] === target) {
+            return [i - 1, i];
+        }
+    }
+    return [-1, -1];
+}
+
+function move(array, from, to) {
+    if (from >= 0 || to >= 0) {
+        const tempFrom = array[from];
+        array[from] = array[to];
+        array[to] = tempFrom;
+    }
+    return array;
 }
