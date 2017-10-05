44441f25e15133103407a2f608a29770e317026b
Delvin Defoe <defoe@rose-hulman.edu>
2017-10-05 12:43:00 -0400

    formsAndAJAX posted

---
+++ formsAndAJAX/js/main.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+
+    let responseDiv;
+
+    $(document).ready(function () {
+        //load in initial state
+        responseDiv = $('#search-by-title-response');
+    });
+})();
