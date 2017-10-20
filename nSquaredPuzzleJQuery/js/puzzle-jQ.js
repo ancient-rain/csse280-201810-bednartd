4de6d4feaefacd6d64b9af6a74183fbf64ea40cf
askalerion <bednartd@rose-hulman.edu>
2017-10-19 20:04:53 -0400

    Initial commit

--- nSquaredPuzzleJQuery/js/puzzle-jQ.js
+++ nSquaredPuzzleJQuery/js/puzzle-jQ.js
@@ -5,6 +5,7 @@
     // CONSTANTS
     const BOARD_WIDTH = 500;  // 500 pixels
     const BORDER_SIZE = 2;    // 3 pixels
+    const VICTORY = document.getElementById('victory');
 	
     // OTHER VARS
     let SQUARE_SIZE;
@@ -15,6 +16,7 @@
     let emptySpace;
     let minSwaps;
     let maxSwaps;
+    let gameStarted = false;
 
     function getSize() {
         SIZE = parseInt($('[name="size"]:checked').val());
@@ -28,6 +30,7 @@
     function setup() {
         // Another way to attach an event listener to an element
         $('#sizeChoice').click(getSize);
+        VICTORY.style.visibility = 'hidden';   
     }
 
     // called when the page first loads to create tiles and empty space 
@@ -37,15 +40,18 @@
 
         for (let i = 1; i <= NUM_SQUARES; i++) {
             let nodeName = i;
+
             if (i === NUM_SQUARES) {
                 nodeName = '&nbsp;';
             }
+
             const newSpan = $(`<span> ${nodeName} </span>`).addClass('tile').css({
                 'width': `${SQUARE_SIZE}px`,
                 'height': `${SQUARE_SIZE}px`,
                 'lineHeight': `${SQUARE_SIZE}px`,
                 'border': `${BORDER_SIZE}px solid white`
             });
+
             if (i === NUM_SQUARES) {
                 newSpan.addClass('space');
                 spacePos = NUM_SQUARES - 1;
@@ -54,8 +60,11 @@
 
             tileList.push(newSpan);
             boardDiv.append(newSpan);
+            newSpan.click(moveTile);
         }
 
+        shuffleTiles();
+        gameStarted = true;
     }
 
     function shuffleTiles() {
@@ -64,7 +73,7 @@
         let swapCount = 0;
         while (swapCount < numSwaps) {
             const index = parseInt(Math.random() * NUM_SQUARES);
-            swapCount += moveTile.call(tileList[index]);  // tileList[index] will be "this" inside moveTile.
+            swapCount += moveTile.call(tileList[index][0]);  // tileList[index] will be "this" inside moveTile.
         }
     }
 
@@ -76,7 +85,37 @@
 
     // If clicked tile is next to the empty space, swap them.
     function moveTile() {
+        const pos = getPos($(this));
+        const diffToEmpty = Math.abs(pos - spacePos);
 
+        if ((diffToEmpty == 1 || diffToEmpty == SIZE) && pos != -1) {
+            const tile = tileList[pos];
+            swapDomElements(tile, emptySpace);
+            swapArrayElements(tileList, pos, spacePos);
+            spacePos = pos;
+
+            if (isSolved()) {
+                setTimeout(function () {
+                    VICTORY.style.visibility = 'visible';
+                });
+            } else {
+                VICTORY.style.visibility = 'hidden';
+            }
+
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    function getPos(clickedTile) {
+        const val = parseInt(clickedTile[0].innerText);
+        for (let i = 0; i < tileList.length; i++) {
+            if (parseInt(tileList[i][0].innerText) == val) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     function sameRow(pos1, pos2, rowSize) {
@@ -88,7 +127,23 @@
     }
 
     function isSolved() {
+        if (!gameStarted) {
+            return false;
+        }
 
+        let val = 1;
+
+        for (let i = 0; i < tileList.length - 1; i++) {
+            const tile = parseInt(tileList[i][0].innerText);
+            
+            if (tile <= val && Number.isNaN(tile)) {
+                return false;
+            } else {
+                val = tile + 1;
+            }
+        }
+
+        return true;
     }
 
     // Exchange the locations of two JQuery DOM elements.  
@@ -99,14 +154,16 @@
         const temp = $("<div>");
 
         // insert temp before element1
+        temp.insertBefore(element1);
 
         // move element1 to immediately before element2
-        element1.insertBefore(element2);
+        $(element1).insertBefore(element2);
 
         // move element2 to immediately before where element1 used to be
-        element2.insertBefore(temp);
+        $(element2).insertBefore(temp);
 
         // remove temporary marker node
+        temp.remove();
     }
 
     $(window).on('load', setup);
